b1b10836a1c9b7b287ec2af98af03621c81873aa
ArtLiss <artliss@rambler.ru>
2026-04-29 12:24:45 +0000

    fix: add log method to logger (was causing TypeError in ServerAuth and DirectLogin)

--- ct-app/src/lib/logger.ts
+++ ct-app/src/lib/logger.ts
@@ -1,6 +1,6 @@
 export const logger = {
+  log: (msg: string, ...args: any[]) => console.log(msg, ...args),
   info: (msg: string, ...args: any[]) => console.log(`[INFO] ${msg}`, ...args),
   warn: (msg: string, ...args: any[]) => console.warn(`[WARN] ${msg}`, ...args),
   error: (msg: string, ...args: any[]) => console.error(`[ERROR] ${msg}`, ...args),
 };
-
